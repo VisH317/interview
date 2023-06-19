b4b4b167eecbe4a024cc0e0f1031103b7b5a926c
Vis <vt201916384@gmail.com>
2023-06-19 19:58:06 -0400

    setup fonts

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-    <div class="w-full fixed bg-white opacity-[0.5] border-black border-2 p-10">
-        hola
+    <div class="w-full fixed bg-white border-black border-2 p-5 text-4xl font-semibold flex flex-row gap-10 justify-center items-center">
+        <nuxt-img src="/logo.png" height="70px" width="70px"/>
+        <p class="text-black font-semibold text-4xl">Intervoo</p>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@nuxt/image'],
   devtools: { enabled: true },
   app: {
     head: {
-      link: [{"rel": "icon", type: "image/png", href: "/logo.png"}]
+      link: [{rel: "icon", type: "image/png", href: "/logo.png"}],
+    }
+  },
+  googleFonts: {
+    preconnect: true,
+    families: {
+      'Nunito+Sans': true
     }
   }
 })
