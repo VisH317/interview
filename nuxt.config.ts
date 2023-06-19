d9b6db1e8acd1f108419502eec94e844f79b6937
Vis <vt201916384@gmail.com>
2023-06-19 18:41:42 -0400

    i love nuxtjs so much it is so beautiful i will bow down to it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {}
-        }
-      }
-    }
-  },
-  css: {
-    '@/assets/css/main.css'
-  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+    <h1 class="text-xl">Test</h1>
+    <p>Test 2</p>
+</template>
