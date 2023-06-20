0e0e4e6a9fc790479092dd9e5da89ab76dd238ee
Vis <vt201916384@gmail.com>
2023-06-19 21:32:51 -0400

    navbar complete

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,13 +4,13 @@
 
 <template>
     <div class="w-full fixed bg-white border-black p-5 text-4xl font-semibold flex flex-row gap-10 justify-center items-center">
-        <div class="flex flex-row gap-10 items-center w-3/12">
+        <div class="flex flex-row gap-10 items-center w-3/12 cursor-pointer">
             <nuxt-img src="/logo.png" height="70px" width="70px"/>
             <p class="text-slate-800 font-semibold text-4xl">InterView</p>
         </div>
         <div class="flex flex-row gap-10 items-center w-1/6">
-            <nuxt-link to="/about" class="text-slate-900 font-light text-2xl">About</nuxt-link>
-            <nuxt-link to="/about" class="text-slate-900 font-light text-2xl">Pricing</nuxt-link>
+            <nuxt-link to="/about" class="text-slate-900 font-light text-2xl duration-300 hover:text-slate-400">About</nuxt-link>
+            <nuxt-link to="/about" class="text-slate-900 font-light text-2xl duration-300 hover:text-slate-400">Pricing</nuxt-link>
             <Button :action="click" text="Join Now"/>
         </div>
     </div>
