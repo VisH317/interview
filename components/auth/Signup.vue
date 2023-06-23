f74fa7336b1fb4b8ef95ae7f4beda9b67a160e48
Vis <vt201916384@gmail.com>
2023-06-23 16:04:16 -0400

    signup form ui matches

--- components/auth/Signup.vue
+++ components/auth/Signup.vue
@@ -10,7 +10,7 @@
 
     // form barrier states
     const disabled = ref<boolean>(true)
-    const matching = ref<boolean>(false)
+    const matching = ref<boolean>(true)
 
     watch([email, password, retype], ([e, p, r]) => {
         if(p.length===0 || r.length===0) matching.value = true
@@ -53,14 +53,17 @@
                 <div class="transition-all duration-300 p-[2px] rounded-lg bg-gradient-to-br from-blue-300 to-pink-300 w-[70%]">
                     <input type="password" class="w-full p-5 rounded-md outline-none text-lg" placeholder="Retype Password: " v-model="retype"/>
                 </div>
-                
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-if="!matching">
+                    <strong class="font-bold">Hold up!  </strong>
+                    <span class="block sm:inline">Those passwords dont match</span>
+                </div>
                 <div class="relative inline-flex items-center justify-center w-full">
                     <hr class="w-64 h-px my-8 bg-gray-200 border-[1px] border-slate-300 dark:bg-slate-600">
                     <span class="absolute px-3 font-normal text-slate-600 bg-white mx-auto dark:text-slate-400 " @click="() => { login = true; signup = false }">Sign In Here :)</span>
                 </div>
-                <div class="group relative flex flex-row items-center transition ease-in-out duration-300 hover:-translate-y-1 hover:opacity-[0.85] w-48 h-24">
-                    <button class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-48 h-full absolute"></button>
-                    <div class="group-hover:shadow-lg border-2 border-slate-800 bg-slate-800 text-white duration-300 transition ease-in-out bg-slate-800 text-2xl font-light rounded-lg h-14 w-40 cursor-pointer text-center flex justify-center items-center z-20 absolute left-4 top-5">Sign Up</div>
+                <div :class="`group relative flex flex-row items-center transition ease-in-out duration-300 ${disabled ? '' : 'hover:-translate-y-1 hover:opacity-[0.85]'} w-48 h-24`">
+                    <button :disabled="disabled" class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-48 h-full absolute" v-if="!disabled"></button>
+                    <div :class="` ${disabled ? 'bg-slate-300 cursor-default' : 'group-hover:shadow-lg bg-slate-800 cursor-pointer'} text-white duration-300 transition ease-in-out text-2xl font-light rounded-lg h-14 w-40 text-center flex justify-center items-center z-20 absolute left-4 top-5`">Sign Up</div>
                 </div>
                 
             </div>
