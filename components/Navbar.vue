6dbabb3e31cf49498cac9fa34258ca7131ab2525
Vis <vt201916384@gmail.com>
2023-06-25 15:59:11 -0400

    setup navbar auto routing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+
     const login = useState<boolean>('login-modal')
+    const user = useSupabaseUser()
+    const router = useRouter()
+
+    const click = () => {
+        if(user.value) router.push("/dashboard")
+        else login.value = true
+    }
+
 </script>
 
 <template>
@@ -11,7 +20,7 @@
         <div class="flex flex-row gap-10 items-center w-1/6">
             <nuxt-link to="/about" class="text-slate-900 font-light text-2xl duration-300 hover:text-slate-400">About</nuxt-link>
             <nuxt-link to="/about" class="text-slate-900 font-light text-2xl duration-300 hover:text-slate-400">Pricing</nuxt-link>
-            <Button :action="() => { login=true }" text="Sign In"/>
+            <Button :action="click" text="Sign In"/>
         </div>
     </div>
 </template>
