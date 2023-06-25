c8f0056b2be4c63b084c5f517cb10c6c62f7210f
Vis <vt201916384@gmail.com>
2023-06-25 15:19:36 -0400

    setup dashboard main layout

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+    useHead({
+        title: "Dashboard - InterviewSense"
+    })
 </script>
 
 <template>
-    <div>
-        HELLOOO
+    <div class="w-screen min-h-screen overflow-x-hidden flex flex-row">
+        <div class="flex-none bg-slate-100 h-screen w-96 overflow-x-hidden overflow-y-auto">
+            hallo
+        </div>
+        <div class="bg-white h-screen grow overflow-x-hidden overflow-y-auto">
+            hola
+        </div>
     </div>
 </template>
