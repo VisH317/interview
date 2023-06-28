c88f5952ae14edcac4b589b8fe3e0873c1f90356
Vis <vt201916384@gmail.com>
2023-06-28 15:57:59 -0400

    setup note fetching on editor

--- components/NoteEditor.vue
+++ components/NoteEditor.vue
@@ -1,10 +1,13 @@
 <script setup>
     const currentNote = useState<string | null>("currentNote")
 
+    const { data: note, refresh } = useFetch('/api/noteById', { query: currentNote.value })
+
+
 </script>
 
 <template>
     <div class="flex flex-col w-[1000px] max-w-[80%] gap-10 relative top-60" v-if="currentNote!==null">
-        
+
     </div>
 </template>
--- server/api/note.get.ts
+++ server/api/note.get.ts
@@ -1,9 +1,11 @@
 import prisma from '../../utils/prisma'
 
 
-export default defineEventHandler(async event => {
+export default defineEventHandler(async (event) => {
     const query = getQuery(event)
     console.log("alosifjoaweijflaskdjf")
+    if(query.id===null) return { id: "", title: "", content: "", userid: "" }
+
     const notes = await prisma.note.findMany({
         where: { userid: query.id }
     })
