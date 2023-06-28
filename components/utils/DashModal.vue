4542646f92b1d7edfbc4af9baf954f0fc0f45d1f
Vis <vt201916384@gmail.com>
2023-06-27 22:06:59 -0400

    setup transitions and scroll

---
+++ components/utils/DashModal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+    const { openDef } = defineProps<{
+        openDef: string,
+        width: string,
+        height: string
+    }>()
+
+    const open = useState<boolean>(openDef)
+    console.log(openDef, ": ", open)
+
+</script>
+
+<template>
+    <div :class="`top-0 left-0 w-screen h-screen bg-[rgba(30,41,59,0.6)] fixed z-50 flex justify-center items-center ${!open ? 'hidden' : ''}`" @click="() => open=false">
+        <div :class="`w-[50%] max-w-[${width}] h-[${height}] bg-white rounded-lg overflow-hidden`" @click.stop>
+            <slot/>
+        </div>
+    </div>
+</template>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -13,6 +13,9 @@
     }
 
     onMounted(() => { if(!user.value) router.push("/") })
+
+    
+
 </script>
 
 <template>
@@ -22,26 +25,30 @@
                 <nuxt-img src="/logo.png" height="70px" width="70px"/>
                 <!-- <p class="text-slate-800 font-semibold text-3xl">Interview<span class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300">Sense</span></p> -->
             </div>
-            <p class="flex-none text-slate-400 font-normal text-xl text-center">{{ user.email.split("@")[0] }}'s Notes</p>
+            <p class="flex-none text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300 font-medium text-xl text-center">{{ user.email.split("@")[0] }}'s Notes</p>
             <div class="h-4"/>
             <div class="w-full">
                 <div class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"><div class="inline"><font-awesome-icon icon="fa-solid fa-cog"/></div> Settings</div>
                 <div class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"><div class="inline"><font-awesome-icon icon="fa-solid fa-user"/></div> Account</div>
             </div>
             <div class="flex-none h-4"/>
-            <div class="grow">
-                <div class="p-5">
+            <div class="grow overflow-y-hidden flex flex-col">
+                <div class="p-5 flex-none">
                     <p class="text-slate-600 font-semibold text-2xl">Your Note Pages</p>
                     <hr class="h-[2px] my-4 bg-gray-200 border-0 dark:bg-slate-400 rounded-lg">
                 </div>
-                <NavItem title="test" :id="1"/>
-                <NavItem title="test" :id="1"/>
+                <div class="overflow-y-auto grow">
+                    <NavItem title="test" :id="1"/>
+                    <NavItem title="test" :id="1"/>
+                    <NavItem title="test" :id="1"/>
+                    <NavItem title="test" :id="1"/>
+                </div>
             </div>
             <div class="flex-none py-10">
                 <hr class="h-[2px] w-[90%] mx-auto bg-gray-200 border-0 dark:bg-slate-400 rounded-lg"/>
                 <div class="h-4"/>
-                <div class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4" @click="logout"><div class="inline"><font-awesome-icon icon="fa-solid fa-plus"/></div> Create Note</div>
-                <div class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4" @click="logout"><div class="inline"><font-awesome-icon icon="fa-solid fa-sign-out"/></div> Logout</div>
+                <div class="group w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4" @click="logout"><div class="inline"><font-awesome-icon icon="fa-solid fa-plus" class="group-hover:rotate-[360deg] duration-300"/></div> Create Note</div>
+                <div class="group w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4" @click="logout"><div class="inline"><font-awesome-icon icon="fa-solid fa-sign-out" class="group-hover:translate-x-1 duration-300"/></div> Logout</div>
             </div>
             <div class="flex-none h-8"/>
         </div>
