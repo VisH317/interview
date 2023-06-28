4bebb84b42e14c2465972eb095b656a258ff4554
Vis <vt201916384@gmail.com>
2023-06-28 15:49:42 -0400

    setup notebyid

---
+++ components/NoteEditor.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const currentNote = useState<string | null>("currentNote")
+
+</script>
+
+<template>
+    <div class="flex flex-col w-[1000px] max-w-[80%] gap-10 relative top-60" v-if="currentNote!==null">
+        
+    </div>
+</template>
---
+++ server/api/noteById.get.ts
@@ -0,0 +1,7 @@
+import prisma from '../../utils/prisma'
+
+export default defineEventHandler(async event => {
+    const query = getQuery(event)
+    const note = await prisma.note.findUnique({ where: { id: query.id as string } })  
+    return note
+})
