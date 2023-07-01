5874dfa3343ccd18fec84088906970accebc6a01
Vis <vt201916384@gmail.com>
2023-06-30 21:22:19 -0400

    HEADINGS YAYYYY

--- components/NoteEditor.vue
+++ components/NoteEditor.vue
@@ -1,25 +1,53 @@
 <script setup lang="ts">
     import { useEditor, EditorContent } from '@tiptap/vue-3'
     import StarterKit from '@tiptap/starter-kit'
+    import Heading from '@tiptap/extension-heading'
+    import { mergeAttributes } from '@tiptap/vue-3';
 
     const editor = useEditor({
-    content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
-    extensions: [
-        StarterKit,
-    ],
+        content: '<h1>I’m running Tiptap with Vue.js. 🎉</h1>',
+        extensions: [
+            StarterKit.configure({
+                heading: false
+            }),
+            Heading.configure({ levels: [1, 2] }).extend({
+            levels: [1, 2],
+            renderHTML({ node, HTMLAttributes }) {
+                const level = this.options.levels.includes(node.attrs.level) 
+                    ? node.attrs.level 
+                    : this.options.levels[0]
+                const classes = {
+                    1: 'text-4xl',
+                    2: 'text-2xl',
+                }
+                return [
+                    `h${level}`,
+                    mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+                        class: `${classes[level]}`,
+                    }),
+                    0,
+                ]
+            },
+    })
+        ],
+        editorProps: {
+            attributes: {
+                class: "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none h-full"
+            }
+        }
     })
 
 
     const currentNote = useState<string | null>("currentNote")
 
-    const { data: note, refresh } = useFetch('/api/noteById', { query: currentNote.value })
+    const { data: note, refresh } = useFetch('/api/noteById', { query: { id: currentNote.value as string } })
 
 
 </script>
 
 <template>
-    <div class="flex flex-col w-[1000px] max-w-[80%] gap-10 relative top-60" v-if="currentNote!==null">
+    <div class="flex flex-col w-[1000px] max-w-[80%] gap-10 relative" v-if="currentNote!==null">
         <!-- <h1 class="text-slate-800 text-6xl font-semibold relative left-6">{{ note.title }}</h1> -->
-        <editor-content :editor="editor" />
+        <editor-content :editor="editor" class="h-full outline-none p-5" />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
     useSeoMeta({ title: "Prepare for Interviews and Tests in Minutes - InterviewSense" })
 
+    const user = useSupabaseUser()
+    const router = useRouter()
+
+    watch(user, () => {
+        if(user.value) router.push("/dashboard")
+    })
+
     const login = useState<boolean>("login-modal", () => false)
     const signup = useState<boolean>("signup-modal", () => false)
 
