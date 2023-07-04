0505f03c0f17796fd43fd2fab3ac9e1388ffaf6f
Vis <vt201916384@gmail.com>
2023-07-04 12:54:37 -0400

    setup langchain chain omg that took so long

---
+++ server/api/model/quiz.post.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+    MapReduceDocumentsChain,
+    MapReduceDocumentsChainInput,
+    LLMChain,
+    StuffDocumentsChain,
+} from "langchain/chains"
+import { PromptTemplate, FewShotPromptTemplate } from "langchain"
+import { Document } from "langchain/dist/document"
+import { OpenAIChat } from "langchain/dist/llms/openai-chat"
+
+const llm = new OpenAIChat({ temperature: 0.1 })
+
+// prompt templates
+const template = PromptTemplate.fromTemplate(`
+    Using the following information, create a list of quiz questions on the topic each separated by a newline. You can either create an open ended question with a single line, or a multiple choice question with a line for the question along with a comma-separated list of answers, as well as the correct answer from the choice list
+`)
+
+const examples = [
+    {
+        documentData:
+            "Leonardo DaVinci painted the Mona Lisa\nLeonardo Da Vinci was known for his dramatic and expressive artwork",
+        questionOutput:
+            "Type: multiple choice; Question: Who painted the Mona Lisa? Answers: Leonardo Da Vinci, Bob Ross, Vincent Van Gogh, Joe Bob; Correct answer index: 0\nType: Open Ended; Question: What was Leonardo Da Vinci known for?",
+    },
+    {
+        documentData:
+            "The analytical engine was created by Charles Babbage\nAda Lovelace wrote the first program for the analytical engine",
+        questionOutput:
+            "Type: open ended; Question: What was Charles Babbages main contribution to computer science?\nType: multiple choice; Question: Who wrote the first program for the analytical engine?; Answers: Bob Joe, Joe Bob, Ada Lovelace, Tim Bob; Correct Answer Index: 2",
+    },
+]
+
+const exampleFormatterTemplate = "document data: {documentData}\n\nQuestion Output: {questionOutput}"
+const examplePrompt = new PromptTemplate({
+    inputVariables: ["documentData", "questionOutput"],
+    template: exampleFormatterTemplate,
+})
+
+const prompt = new FewShotPromptTemplate({
+    examples,
+    examplePrompt,
+    prefix: "Using the following information, create a list of quiz questions on the topic each separated by a newline. You can either create an open ended question with a single line, or a multiple choice question with a line for the question along with a comma-separated list of answers, as well as the correct answer from the choice list",
+    suffix: "Document data: {input}\nQuestion Output: ",
+    inputVariables: ["input"],
+    exampleSeparator: "\n\n\n",
+    templateFormat: "f-string",
+})
+
+// this is the individual documents prompt
+const llmChain = new LLMChain({ prompt, llm })
+
+const chain = new StuffDocumentsChain({
+    llmChain,
+    documentVariableName: "input",
+})
