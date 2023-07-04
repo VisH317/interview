48332d77649649c1a4e1e3b2db0a1d83455c2908
Vis <vt201916384@gmail.com>
2023-07-04 14:08:59 -0400

    fixing errors

--- utils/cardsChain.ts
+++ utils/cardsChain.ts
@@ -1,52 +1,54 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { LLMChain, StuffDocumentsChain } from "langchain/chains"
-import { PromptTemplate, FewShotPromptTemplate } from "langchain"
-import { OpenAI } from "langchain/llms/openai"
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { LLMChain, StuffDocumentsChain } from "langchain/chains"
+// import { PromptTemplate, FewShotPromptTemplate } from "langchain"
+// import { OpenAI } from "langchain/llms/openai"
 
-const llm = new OpenAI({
-    temperature: 0.1,
-    modelName: "gpt-3.5-turbo",
-    openAIApiKey: process.env.OPENAI_KEY,
-})
+// const runtimeConfig = useRuntimeConfig()
 
-const examples = [
-    {
-        documentData:
-            "Leonardo DaVinci painted the Mona Lisa\nLeonardo Da Vinci was known for his dramatic and expressive artwork\nVincent Van Gogh was a close collaborator of Da Vinci",
-        questionOutput:
-            "Leonardo Da Vinci: painter of the Mona Lisa, Vincent Van Gogh: Worked with Leonardo Da Vinci",
-    },
-    {
-        documentData:
-            "The analytical engine was created by Charles Babbage\nAda Lovelace wrote the first program for the analytical engine",
-        questionOutput:
-            "Charles Babbage: creator of the analytical engine, Ada Lovelace: first programmer of the analytical engine",
-    },
-]
+// const llm = new OpenAI({
+//     temperature: 0.1,
+//     modelName: "gpt-3.5-turbo",
+//     openAIApiKey: runtimeConfig.openaiKey,
+// })
 
-const exampleFormatterTemplate =
-    "document data: {documentData}\nFlashcards: {questionOutput}"
-const examplePrompt = new PromptTemplate({
-    inputVariables: ["documentData", "questionOutput"],
-    template: exampleFormatterTemplate,
-})
+// const examples = [
+//     {
+//         documentData:
+//             "Leonardo DaVinci painted the Mona Lisa\nLeonardo Da Vinci was known for his dramatic and expressive artwork\nVincent Van Gogh was a close collaborator of Da Vinci",
+//         questionOutput:
+//             "Leonardo Da Vinci: painter of the Mona Lisa, Vincent Van Gogh: Worked with Leonardo Da Vinci",
+//     },
+//     {
+//         documentData:
+//             "The analytical engine was created by Charles Babbage\nAda Lovelace wrote the first program for the analytical engine",
+//         questionOutput:
+//             "Charles Babbage: creator of the analytical engine, Ada Lovelace: first programmer of the analytical engine",
+//     },
+// ]
 
-const prompt = new FewShotPromptTemplate({
-    examples,
-    examplePrompt,
-    prefix: "Using the following information, create a list of flashcards that represent key terms in the provided input document. Your output should contain a list of key value pairs separated by commas. Each key value pair includes a term as the key and a description or answer for that term as the value.",
-    suffix: "Document data: {input}\nFlashcards: ",
-    inputVariables: ["input"],
-    exampleSeparator: "\n\n\n",
-    templateFormat: "f-string",
-})
+// const exampleFormatterTemplate =
+//     "document data: {documentData}\nFlashcards: {questionOutput}"
+// const examplePrompt = new PromptTemplate({
+//     inputVariables: ["documentData", "questionOutput"],
+//     template: exampleFormatterTemplate,
+// })
 
-// this is the individual documents prompt
-const llmChain = new LLMChain({ prompt, llm })
+// const prompt = new FewShotPromptTemplate({
+//     examples,
+//     examplePrompt,
+//     prefix: "Using the following information, create a list of flashcards that represent key terms in the provided input document. Your output should contain a list of key value pairs separated by commas. Each key value pair includes a term as the key and a description or answer for that term as the value.",
+//     suffix: "Document data: {input}\nFlashcards: ",
+//     inputVariables: ["input"],
+//     exampleSeparator: "\n\n\n",
+//     templateFormat: "f-string",
+// })
 
-const chain = new StuffDocumentsChain({
-    llmChain,
-    documentVariableName: "input",
-})
+// // this is the individual documents prompt
+// const llmChain = new LLMChain({ prompt, llm })
 
-export default chain
+// const chain = new StuffDocumentsChain({
+//     llmChain,
+//     documentVariableName: "input",
+// })
+
+// export default chain
--- utils/quizChain.ts
+++ utils/quizChain.ts
@@ -1,52 +1,54 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { LLMChain, StuffDocumentsChain } from "langchain/chains"
-import { PromptTemplate, FewShotPromptTemplate } from "langchain"
-import { OpenAI } from "langchain/llms/openai"
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { LLMChain, StuffDocumentsChain } from "langchain/chains"
+// import { PromptTemplate, FewShotPromptTemplate } from "langchain"
+// import { OpenAI } from "langchain/llms/openai"
 
-const llm = new OpenAI({
-    temperature: 0.1,
-    modelName: "gpt-3.5-turbo",
-    openAIApiKey: process.env.OPENAI_KEY,
-})
+// const runtimeConfig = useRuntimeConfig()
 
-const examples = [
-    {
-        documentData:
-            "Leonardo DaVinci painted the Mona Lisa\nLeonardo Da Vinci was known for his dramatic and expressive artwork",
-        questionOutput:
-            "Type: multiple choice; Question: Who painted the Mona Lisa? Answers: Leonardo Da Vinci, Bob Ross, Vincent Van Gogh, Joe Bob; Correct answer index: 0\nType: Open Ended; Question: What was Leonardo Da Vinci known for?",
-    },
-    {
-        documentData:
-            "The analytical engine was created by Charles Babbage\nAda Lovelace wrote the first program for the analytical engine",
-        questionOutput:
-            "Type: open ended; Question: What was Charles Babbages main contribution to computer science?\nType: multiple choice; Question: Who wrote the first program for the analytical engine?; Answers: Bob Joe, Joe Bob, Ada Lovelace, Tim Bob; Correct Answer Index: 2",
-    },
-]
+// const llm = new OpenAI({
+//     temperature: 0.1,
+//     modelName: "gpt-3.5-turbo",
+//     openAIApiKey: runtimeConfig.openaiKey,
+// })
 
-const exampleFormatterTemplate =
-    "document data: {documentData}\n\nQuestion Output: {questionOutput}"
-const examplePrompt = new PromptTemplate({
-    inputVariables: ["documentData", "questionOutput"],
-    template: exampleFormatterTemplate,
-})
+// const examples = [
+//     {
+//         documentData:
+//             "Leonardo DaVinci painted the Mona Lisa\nLeonardo Da Vinci was known for his dramatic and expressive artwork",
+//         questionOutput:
+//             "Type: multiple choice; Question: Who painted the Mona Lisa? Answers: Leonardo Da Vinci, Bob Ross, Vincent Van Gogh, Joe Bob; Correct answer index: 0\nType: Open Ended; Question: What was Leonardo Da Vinci known for?",
+//     },
+//     {
+//         documentData:
+//             "The analytical engine was created by Charles Babbage\nAda Lovelace wrote the first program for the analytical engine",
+//         questionOutput:
+//             "Type: open ended; Question: What was Charles Babbages main contribution to computer science?\nType: multiple choice; Question: Who wrote the first program for the analytical engine?; Answers: Bob Joe, Joe Bob, Ada Lovelace, Tim Bob; Correct Answer Index: 2",
+//     },
+// ]
 
-const prompt = new FewShotPromptTemplate({
-    examples,
-    examplePrompt,
-    prefix: "Using the following information, create a list of quiz questions on the topic each separated by a newline. You can either create an open ended question with a single line, or a multiple choice question with a line for the question along with a comma-separated list of answers, as well as the correct answer from the choice list",
-    suffix: "Document data: {input}\nQuestion Output: ",
-    inputVariables: ["input"],
-    exampleSeparator: "\n\n\n",
-    templateFormat: "f-string",
-})
+// const exampleFormatterTemplate =
+//     "document data: {documentData}\n\nQuestion Output: {questionOutput}"
+// const examplePrompt = new PromptTemplate({
+//     inputVariables: ["documentData", "questionOutput"],
+//     template: exampleFormatterTemplate,
+// })
 
-// this is the individual documents prompt
-const llmChain = new LLMChain({ prompt, llm })
+// const prompt = new FewShotPromptTemplate({
+//     examples,
+//     examplePrompt,
+//     prefix: "Using the following information, create a list of quiz questions on the topic each separated by a newline. You can either create an open ended question with a single line, or a multiple choice question with a line for the question along with a comma-separated list of answers, as well as the correct answer from the choice list",
+//     suffix: "Document data: {input}\nQuestion Output: ",
+//     inputVariables: ["input"],
+//     exampleSeparator: "\n\n\n",
+//     templateFormat: "f-string",
+// })
 
-const chain = new StuffDocumentsChain({
-    llmChain,
-    documentVariableName: "input",
-})
+// // this is the individual documents prompt
+// const llmChain = new LLMChain({ prompt, llm })
 
-export default chain
+// const chain = new StuffDocumentsChain({
+//     llmChain,
+//     documentVariableName: "input",
+// })
+
+// export default chain
