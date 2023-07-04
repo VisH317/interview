8dab3e75e80dd6c37d8eacdc626b46b04e125422
Vis <vt201916384@gmail.com>
2023-07-04 14:37:00 -0400

    fix parsing on content

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -45,11 +45,10 @@ const createNote = async () => {
         $fetch("/api/todo", { method: "POST", body: { desc: desc.value } })
     )
 
-    const content = `<h1>${title.value}</h1><h2>Your Summary:</h2><p>${
-        description.value
-    }</p><br><h2>Your Tasks: </h2> <ul>${(todo.value as string[]).map(
-        (t) => "<li>" + t + "</li>"
-    )}</ul>`
+    const content = `<h1>${title.value}</h1><h2>Your Summary:</h2><p>${description.value
+    }</p><br><h2>Your Tasks: </h2> <ul>${(todo.value as string[])
+        .filter((t) => t.length >= 3)
+        .map((t) => "<li>" + t + "</li>")}</ul>`
 
     await $fetch("/api/note", {
         method: "POST",
@@ -88,34 +87,26 @@ const deleteNote = async () => {
 <template>
     <div>
         <div class="w-screen min-h-screen overflow-x-hidden flex flex-row">
-            <div
-                class="flex-none bg-slate-100 h-screen w-[260px] overflow-x-hidden overflow-y-auto flex flex-col"
-            >
-                <div
-                    class="flex-none flex flex-row gap-3 items-center w-full cursor-pointer p-5"
-                >
+            <div class="flex-none bg-slate-100 h-screen w-[260px] overflow-x-hidden overflow-y-auto flex flex-col">
+                <div class="flex-none flex flex-row gap-3 items-center w-full cursor-pointer p-5">
                     <nuxt-img src="/logo.png" height="40px" width="40px" />
                     <p class="text-slate-800 font-semibold text-2xl">
                         Interview<span
-                            class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300"
-                            >Sense</span
-                        >
+                            class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300">Sense</span>
                     </p>
                 </div>
                 <!-- <p class="flex-none text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300 font-medium text-xl text-center">{{ user?.email?.split("@")[0] }}'s Notes</p> -->
                 <div class="h-4" />
                 <div class="w-full">
                     <div
-                        class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"
-                    >
+                        class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4">
                         <div class="inline">
                             <font-awesome-icon icon="fa-solid fa-cog" />
                         </div>
                         Settings
                     </div>
                     <div
-                        class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"
-                    >
+                        class="w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4">
                         <div class="inline">
                             <font-awesome-icon icon="fa-solid fa-user" />
                         </div>
@@ -130,51 +121,31 @@ const deleteNote = async () => {
                         </p>
                     </div>
                     <div class="overflow-y-auto grow">
-                        <NavItem
-                            v-for="(note, index) in notes"
-                            :id="index"
-                            :key="index"
-                            :uuid="note.id"
-                            :title="note.title"
-                            @delete="() => setDelete(index)"
-                        />
+                        <NavItem v-for="(note, index) in notes" :id="index" :key="index" :uuid="note.id" :title="note.title"
+                            @delete="() => setDelete(index)" />
                     </div>
                 </div>
                 <div class="flex-none py-10">
-                    <hr
-                        class="h-[2px] w-[90%] mx-auto bg-gray-200 border-0 dark:bg-slate-400 rounded-lg"
-                    />
+                    <hr class="h-[2px] w-[90%] mx-auto bg-gray-200 border-0 dark:bg-slate-400 rounded-lg" />
                     <div class="h-4" />
-                    <div
-                        class="group w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"
-                        @click="() => (open = true)"
-                    >
+                    <div class="group w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"
+                        @click="() => (open = true)">
                         <div class="inline">
-                            <font-awesome-icon
-                                icon="fa-solid fa-plus"
-                                class="group-hover:rotate-[360deg] duration-300"
-                            />
+                            <font-awesome-icon icon="fa-solid fa-plus" class="group-hover:rotate-[360deg] duration-300" />
                         </div>
                         Create Note
                     </div>
-                    <div
-                        class="group w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"
-                        @click="logout"
-                    >
+                    <div class="group w-full p-5 hover:bg-slate-300 duration-300 text-slate-400 font-normal text-xl cursor-pointer flex gap-4"
+                        @click="logout">
                         <div class="inline">
-                            <font-awesome-icon
-                                icon="fa-solid fa-sign-out"
-                                class="group-hover:translate-x-1 duration-300"
-                            />
+                            <font-awesome-icon icon="fa-solid fa-sign-out" class="group-hover:translate-x-1 duration-300" />
                         </div>
                         Logout
                     </div>
                 </div>
                 <div class="flex-none h-8" />
             </div>
-            <div
-                class="bg-white h-screen grow overflow-x-hidden overflow-y-auto"
-            >
+            <div class="bg-white h-screen grow overflow-x-hidden overflow-y-auto">
                 <Main />
             </div>
         </div>
@@ -182,48 +153,28 @@ const deleteNote = async () => {
             <div class="p-10 flex flex-col gap-10 items-center justify-center">
                 <h1 class="text-slate-800 font-semibold text-4xl">
                     Let's Get
-                    <span
-                        class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300"
-                        >Started</span
-                    >
+                    <span class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300">Started</span>
                 </h1>
                 <div class="flex justify-center items-center w-full gap-3">
-                    <div
-                        class="flex flex-col items-center justify-center w-[60%] p-5 gap-5"
-                    >
-                        <input
-                            v-model="title"
-                            type="text"
+                    <div class="flex flex-col items-center justify-center w-[60%] p-5 gap-5">
+                        <input v-model="title" type="text"
                             class="w-full p-4 outline-none hover:border-slate-400 focus:border-slate-400 duration-300 border-2 border-slate-300 rounded-lg text-xl h-14"
-                            placeholder="Title: "
-                        />
-                        <textarea
-                            v-model="desc"
-                            type="text"
+                            placeholder="Title: " />
+                        <textarea v-model="desc" type="text"
                             class="w-full p-4 outline-none hover:border-slate-400 focus:border-slate-400 duration-300 border-2 border-slate-300 rounded-lg text-xl h-40"
-                            placeholder="Paste in the job description of the job you're applying to here: "
-                        />
+                            placeholder="Paste in the job description of the job you're applying to here: " />
                     </div>
-                    <div
-                        :class="`group relative flex flex-row items-center transition ease-in-out duration-300 ${
-                            disabled
-                                ? ''
-                                : 'hover:-translate-y-1 hover:opacity-[0.85]'
-                        } w-40 h-24`"
-                        @click="() => void createNote()"
-                    >
-                        <button
-                            v-if="!disabled"
-                            :disabled="disabled"
-                            class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-40 h-full absolute"
-                        ></button>
+                    <div :class="`group relative flex flex-row items-center transition ease-in-out duration-300 ${disabled
+                            ? ''
+                            : 'hover:-translate-y-1 hover:opacity-[0.85]'
+                        } w-40 h-24`" @click="() => void createNote()">
+                        <button v-if="!disabled" :disabled="disabled"
+                            class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-40 h-full absolute"></button>
                         <div
-                            :class="` ${
-                                disabled
+                            :class="` ${disabled
                                     ? 'bg-slate-300 cursor-default'
                                     : 'group-hover:shadow-lg bg-slate-800 cursor-pointer'
-                            } text-white duration-300 transition ease-in-out text-2xl font-light rounded-lg h-14 w-32 text-center flex justify-center items-center z-20 absolute left-4 top-5`"
-                        >
+                                } text-white duration-300 transition ease-in-out text-2xl font-light rounded-lg h-14 w-32 text-center flex justify-center items-center z-20 absolute left-4 top-5`">
                             Create
                         </div>
                     </div>
@@ -236,26 +187,18 @@ const deleteNote = async () => {
                     Are you sure you want to delete
                     {{ deleteIndex > -1 ? notes![deleteIndex].title : "" }}?
                 </h1>
-                <div
-                    class="flex justify-center items-center w-full items-center gap-3"
-                >
-                    <div
-                        :class="`group relative flex flex-row items-center transition ease-in-out duration-300 hover:-translate-y-1 hover:opacity-[0.85] w-40 h-24`"
-                        @click="() => (deleteOpen = false)"
-                    >
+                <div class="flex justify-center items-center w-full items-center gap-3">
+                    <div :class="`group relative flex flex-row items-center transition ease-in-out duration-300 hover:-translate-y-1 hover:opacity-[0.85] w-40 h-24`"
+                        @click="() => (deleteOpen = false)">
                         <button
-                            :class="`text-slate-400 border-2 border-slate-400 duration-300 font-semibold transition ease-in-out text-2xl font-light rounded-lg h-14 w-32 text-center flex justify-center items-center z-20 absolute left-4 top-5`"
-                        >
+                            :class="`text-slate-400 border-2 border-slate-400 duration-300 font-semibold transition ease-in-out text-2xl font-light rounded-lg h-14 w-32 text-center flex justify-center items-center z-20 absolute left-4 top-5`">
                             No
                         </button>
                     </div>
-                    <div
-                        :class="`group relative flex flex-row items-center transition ease-in-out duration-300 hover:-translate-y-1 hover:opacity-[0.85] w-40 h-24`"
-                        @click="() => void deleteNote()"
-                    >
+                    <div :class="`group relative flex flex-row items-center transition ease-in-out duration-300 hover:-translate-y-1 hover:opacity-[0.85] w-40 h-24`"
+                        @click="() => void deleteNote()">
                         <button
-                            :class="` text-white duration-300 bg-red-500  transition ease-in-out text-2xl font-light rounded-lg h-14 w-32 text-center flex justify-center items-center z-20 absolute left-4 top-5`"
-                        >
+                            :class="` text-white duration-300 bg-red-500  transition ease-in-out text-2xl font-light rounded-lg h-14 w-32 text-center flex justify-center items-center z-20 absolute left-4 top-5`">
                             Yes
                         </button>
                     </div>
