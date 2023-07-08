fae43646aaee6370172a821c3ee230227c9d91e4
Vis <vt201916384@gmail.com>
2023-07-07 22:21:17 -0400

    setup quiz viewing

--- components/tools/Quiz.vue
+++ components/tools/Quiz.vue
@@ -115,6 +115,7 @@ const submitQuiz = async () => {
     })
 
     quizQuestions.value = newQuestions
+    currentQuiz.value = "graded"
 }
 </script>
 
@@ -149,6 +150,19 @@ const submitQuiz = async () => {
                             class="transition-all duration-300 p-[2px] rounded-lg bg-gradient-to-br from-blue-300 to-pink-300 w-[70%]">
                             <textarea type="text" class="w-full p-5 rounded-md outline-none text-lg"
                                 v-model="formStates[ix]" placeholder="Email: " style="resize: none" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        class="group relative flex flex-row items-center transition ease-in-out duration-300 hover:-translate-y-1 hover:opacity-[0.85] w-48 h-24">
+                        <button
+                            class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-48 h-full absolute"></button>
+                        <div
+                            class="group-hover:shadow-lg duration-300 transition ease-in-out bg-slate-800 text-white text-2xl font-light py-3 px-5 rounded-lg items-center h-14 w-40 cursor-pointer text-center z-20 absolute left-4 top-5"
+                            @click="submitQuiz"
+                        >
+                            Submit Quiz
                         </div>
                     </div>
                 </div>
