beee7de744758e47c008569198fd4caead04acdd
Vis <vt201916384@gmail.com>
2023-07-08 16:50:25 -0400

    flashcards complete

--- components/tools/Card.vue
+++ components/tools/Card.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-const props = defineProps<{}>()
+const props = defineProps<{
+    term: string
+    def: string
+}>()
 
 type rotate = "rotateY(0deg)" | "rotateY(-180deg)"
 
@@ -22,18 +25,18 @@ const flipFront = () => {
         :class="`w-full relative h-full overflow-hidden ${hidden ? 'hidden' : ''}`"
     >
         <div
-            class="p-20 rounded-lg w-full h-full overflow-hidden duration-300 border-2 bg-slate-100 absolute top-0 left-0"
+            class="p-20 rounded-lg w-full h-full overflow-hidden duration-300 border-2 bg-slate-100 absolute top-0 left-0 text-4xl text-slate-800 font-semibold flex justify-center items-center"
             :style="`transform-style: preserve-3d; transform: ${r}; backface-visibility: hidden`"
             @click="flipBack"
         >
-            test
+            {{ term }}
         </div>
         <div
-            class="p-20 rounded-lg w-full h-full overflow-hidden duration-300 border-2 bg-slate-600 absolute top-0 left-0"
+            class="p-20 rounded-lg w-full h-full overflow-hidden duration-300 border-2 bg-slate-600 absolute top-0 left-0 text-xl text-slate-200 font-normal flex justify-center items-center"
             :style="`transform-style: preserve-3d; transform: ${b}; backface-visibility: hidden`"
             @click="flipFront"
         >
-            test
+            {{ def }}
         </div>
     </div>
 </template>
