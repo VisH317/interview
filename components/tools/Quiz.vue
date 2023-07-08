b2d0ae51e4a220f430350c69422ea70c1ee69956
Vis <vt201916384@gmail.com>
2023-07-07 22:13:02 -0400

    setup grading answer implementation

--- components/tools/Quiz.vue
+++ components/tools/Quiz.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-
 interface OpenEnded {
     question: string
+    answer: string | null
 }
 
 interface MultipleChoice {
@@ -48,22 +48,25 @@ const createQuiz = async () => {
 
     quizText.value = quizData.value
 
-    quizText.value.map(t => {
+    quizText.value.map((t) => {
         const value: string[] = t.split(";")
         if (value[0]?.includes("open") || value[0]?.includes("ended")) {
-            const ret: OpenEnded = { question: value[1]?.replace(/^\s+|\s+$/g, '') }
+            const ret: OpenEnded = {
+                question: value[1]?.replace(/^\s+|\s+$/g, ""),
+                answer: null,
+            }
             quizQuestions.value = [...quizQuestions.value, ret]
             formStates.value = [...formStates.value, ""]
             return
         }
 
         const answers = value[2]?.split(",")
-        answers.map((a, ix) => answers[ix] = a.replace(/^\s+|\s+$/g, ''))
+        answers.map((a, ix) => (answers[ix] = a.replace(/^\s+|\s+$/g, "")))
 
         const ret: MultipleChoice = {
-            question: value[1]?.replace(/^\s+|\s+$/g, ''),
+            question: value[1]?.replace(/^\s+|\s+$/g, ""),
             answers,
-            correct: Number(value[3]?.replace(/^\s+|\s+$/g, ''))
+            correct: Number(value[3]?.replace(/^\s+|\s+$/g, "")),
         }
 
         formStates.value = [...formStates.value, -1]
@@ -73,13 +76,54 @@ const createQuiz = async () => {
     currentQuiz.value = true
     loading.value = false
 }
+
+const submitQuiz = async () => {
+    const arr = quizQuestions.value.filter((q) => !("answers" in q))
+    const req: string[] = []
+    // eslint-disable-next-line array-callback-return
+    arr.map((q, ix) => {
+        req.push(
+            ("Question: " +
+                q.question +
+                " Answer: " +
+                formStates.value[ix]) as string
+        )
+    })
+
+    const { data: answers } = await useAsyncData(
+        `grade_quiz_${note.value.id}`,
+        () =>
+            $fetch("/api/grade", {
+                method: "POST",
+                body: {
+                    text: note.value?.content,
+                    questions: req,
+                },
+            })
+    )
+
+    let it = 0;
+
+    const newQuestions = [...quizQuestions.value]
+
+    //eslint-disable-next-line array-callback-return
+    quizQuestions.value.map((q) => {
+        if ("answer" in q && q.answer === null) {
+            q.answer = answers.value[it]
+            it++
+        }
+    })
+
+    quizQuestions.value = newQuestions
+}
 </script>
 
 <template>
     <NoteModal open-def="quiz">
-        <div
-            :class="`w-full h-full absolute bg-[rgba(30,41,59,0.7)] z-50 flex justify-center items-center text-4xl font-semibold ${loading ? '' : 'hidden'}`">
-            LOADING</div>
+        <div :class="`w-full h-full absolute bg-[rgba(30,41,59,0.7)] z-50 flex justify-center items-center text-4xl font-semibold ${loading ? '' : 'hidden'
+            }`">
+            LOADING
+        </div>
         <div v-if="currentQuiz" class="p-5 py-10 flex flex-col items-center gap-5">
             <div class="flex-none">
                 <h2 class="text-4xl text-slate-400 font-semibold">
@@ -89,13 +133,11 @@ const createQuiz = async () => {
             <div class="grow flex flex-col gap-5 items-center">
                 <div v-for="(question, ix) in quizQuestions">
                     <div v-if="'answers' in question" class="flex flex-col gap-2">
-                        <p class="text-xl text-slate-800">{{ question.question }}</p>
+                        <p class="text-xl text-slate-800">
+                            {{ question.question }}
+                        </p>
                         <div v-for="(answer, idx) in question.answers">
-                            <input
-                                v-model="formStates[ix]"
-                                type="radio"
-                                :value="idx"
-                            />
+                            <input v-model="formStates[ix]" type="radio" :value="idx" />
                             {{ answer }}
                         </div>
                     </div>
@@ -105,13 +147,8 @@ const createQuiz = async () => {
                         </p>
                         <div
                             class="transition-all duration-300 p-[2px] rounded-lg bg-gradient-to-br from-blue-300 to-pink-300 w-[70%]">
-                            <textarea
-                                type="text"
-                                class="w-full p-5 rounded-md outline-none text-lg"
-                                v-model="formStates[ix]"
-                                placeholder="Email: "
-                                style="resize: none"
-                            />
+                            <textarea type="text" class="w-full p-5 rounded-md outline-none text-lg"
+                                v-model="formStates[ix]" placeholder="Email: " style="resize: none" />
                         </div>
                     </div>
                 </div>
@@ -138,4 +175,5 @@ const createQuiz = async () => {
                 </div>
             </div>
         </div>
-</NoteModal></template>
+    </NoteModal>
+</template>
