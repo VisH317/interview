7a43202f6468baafcbe42d700ab16f361befb16e
Vis <vt201916384@gmail.com>
2023-07-08 14:08:25 -0400

    prompt config

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -1,38 +1,38 @@
 <script setup lang="ts">
 
-    const router = useRouter()
-    const user = useSupabaseUser()
+const router = useRouter()
+const user = useSupabaseUser()
 
-    const supabase = useSupabaseClient()
+const supabase = useSupabaseClient()
 
-    const login = useState<boolean>('login-modal')
-    const signup = useState<boolean>('signup-modal')
+const login = useState<boolean>('login-modal')
+const signup = useState<boolean>('signup-modal')
 
-    // form states (ig use ref for local)
-    const email = ref<string>("")
-    const password = ref<string>("")
+// form states (ig use ref for local)
+const email = ref<string>("")
+const password = ref<string>("")
 
-    const disabled = ref<boolean>(true)
+const disabled = ref<boolean>(true)
 
-    watch([email, password], ([e, p]) => {
-        if(e.length===0 || p.length===0) disabled.value = true
-        else disabled.value = false
-    })
+watch([email, password], ([e, p]) => {
+    if (e.length === 0 || p.length === 0) disabled.value = true
+    else disabled.value = false
+})
 
 
-    const onSubmit = async () => {
-        console.log("SUBMITTING")
-        const user = await supabase.auth.signInWithPassword({ email: email.value, password: password.value })
-        console.log("HEELO")
-        if(user.error) alert("Email and password do not match")
-        router.push("/dashboard")
-    }
+const onSubmit = async () => {
+    console.log("SUBMITTING")
+    const user = await supabase.auth.signInWithPassword({ email: email.value, password: password.value })
+    console.log("HEELO")
+    if (user.error) alert("Email and password do not match")
+    await router.push("/dashboard")
+}
 
-    // onMounted(() => {
-    //     if(user.value) router.push("/dashboard")
-    // })
+// onMounted(() => {
+//     if(user.value) router.push("/dashboard")
+// })
 
-    
+
 </script>
 
 <template>
@@ -40,8 +40,10 @@
         <div class="w-full h-full flex justify-center">
             <div class="w-[40%] h-full bg-slate-700 p-5 flex flex-col justify-center items-center gap-4">
                 <div class="flex justify-center items-center gap-4">
-                    <nuxt-img src="/logo.png" height="70px" width="70px"/>
-                    <p class="text-slate-100 font-semibold text-4xl">Interview<span class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300">Sense</span></p>
+                    <nuxt-img src="/logo.png" height="70px" width="70px" />
+                    <p class="text-slate-100 font-semibold text-4xl">Interview<span
+                            class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300">Sense</span>
+                    </p>
                 </div>
                 <!-- <div class="relative inline-flex items-center justify-center w-full">
                     <hr class="w-64 h-px my-8 bg-gray-200 border-[1px] border-slate-300 dark:bg-slate-600">
@@ -50,24 +52,33 @@
             </div>
             <div class="w-[60%] flex flex-col items-center justify-center gap-4">
                 <h3 class="text-6xl font-bold text-slate-800">Welcome Back</h3>
-                <div class="h-12"/>
-                <div class="transition-all duration-300 p-[2px] rounded-lg bg-gradient-to-br from-blue-300 to-pink-300 w-[70%]">
-                    <input type="email" class="w-full p-5 rounded-md outline-none text-lg" placeholder="Email: " v-model="email"/>
+                <div class="h-12" />
+                <div
+                    class="transition-all duration-300 p-[2px] rounded-lg bg-gradient-to-br from-blue-300 to-pink-300 w-[70%]">
+                    <input type="email" class="w-full p-5 rounded-md outline-none text-lg" placeholder="Email: "
+                        v-model="email" />
                 </div>
-                <div class="h-6"/>
-                <div class="transition-all duration-300 p-[2px] rounded-lg bg-gradient-to-br from-blue-300 to-pink-300 w-[70%]">
-                    <input type="password" class="w-full p-5 rounded-md outline-none text-lg" placeholder="Password: " v-model="password"/>
+                <div class="h-6" />
+                <div
+                    class="transition-all duration-300 p-[2px] rounded-lg bg-gradient-to-br from-blue-300 to-pink-300 w-[70%]">
+                    <input type="password" class="w-full p-5 rounded-md outline-none text-lg" placeholder="Password: "
+                        v-model="password" />
                 </div>
                 <div class="relative inline-flex items-center justify-center w-full">
                     <hr class="w-80 h-px my-8 bg-gray-200 border-[1px] border-slate-300 dark:bg-slate-600">
-                    <span class="absolute px-3 font-normal text-slate-600 bg-white mx-auto dark:text-slate-400 " @click="() => { login = false; signup = true }">Sign Up Here :)</span>
+                    <span class="absolute px-3 font-normal text-slate-600 bg-white mx-auto dark:text-slate-400 "
+                        @click="() => { login = false; signup = true }">Sign Up Here :)</span>
                 </div>
-                <div :class="`group relative flex flex-row items-center transition ease-in-out duration-300 ${disabled ? '' : 'hover:-translate-y-1 hover:opacity-[0.85]'} w-48 h-24`" @click="() => void onSubmit()">
-                    <button :disabled="disabled" class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-48 h-full absolute" v-if="!disabled"></button>
-                    <div :class="` ${disabled ? 'bg-slate-300 cursor-default' : 'group-hover:shadow-lg bg-slate-800 cursor-pointer'} text-white duration-300 transition ease-in-out text-2xl font-light rounded-lg h-14 w-40 text-center flex justify-center items-center z-20 absolute left-4 top-5`">Sign Up</div>
-                </div>
-                
+                <div :class="`group relative flex flex-row items-center transition ease-in-out duration-300 ${disabled ? '' : 'hover:-translate-y-1 hover:opacity-[0.85]'} w-48 h-24`"
+                    @click="() => void onSubmit()">
+                    <button :disabled="disabled"
+                    class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-48 h-full absolute"
+                    v-if="!disabled"></button>
+                <div
+                    :class="` ${disabled ? 'bg-slate-300 cursor-default' : 'group-hover:shadow-lg bg-slate-800 cursor-pointer'} text-white duration-300 transition ease-in-out text-2xl font-light rounded-lg h-14 w-40 text-center flex justify-center items-center z-20 absolute left-4 top-5`">
+                    Sign Up</div>
             </div>
+
         </div>
-    </Modal>
-</template>
+    </div>
+</Modal></template>
--- utils/cardsChain.ts
+++ utils/cardsChain.ts
@@ -1,54 +1,54 @@
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { LLMChain, StuffDocumentsChain } from "langchain/chains"
-// import { PromptTemplate, FewShotPromptTemplate } from "langchain"
-// import { OpenAI } from "langchain/llms/openai"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { LLMChain, StuffDocumentsChain } from "langchain/chains"
+import { PromptTemplate, FewShotPromptTemplate } from "langchain"
+import { OpenAI } from "langchain/llms/openai"
 
-// const runtimeConfig = useRuntimeConfig()
+const runtimeConfig = useRuntimeConfig()
 
-// const llm = new OpenAI({
-//     temperature: 0.1,
-//     modelName: "gpt-3.5-turbo",
-//     openAIApiKey: runtimeConfig.openaiKey,
-// })
+const llm = new OpenAI({
+    temperature: 0.1,
+    modelName: "gpt-3.5-turbo",
+    openAIApiKey: runtimeConfig.openaiKey,
+})
 
-// const examples = [
-//     {
-//         documentData:
-//             "Leonardo DaVinci painted the Mona Lisa\nLeonardo Da Vinci was known for his dramatic and expressive artwork\nVincent Van Gogh was a close collaborator of Da Vinci",
-//         questionOutput:
-//             "Leonardo Da Vinci: painter of the Mona Lisa, Vincent Van Gogh: Worked with Leonardo Da Vinci",
-//     },
-//     {
-//         documentData:
-//             "The analytical engine was created by Charles Babbage\nAda Lovelace wrote the first program for the analytical engine",
-//         questionOutput:
-//             "Charles Babbage: creator of the analytical engine, Ada Lovelace: first programmer of the analytical engine",
-//     },
-// ]
+const examples = [
+    {
+        documentData:
+            "Leonardo DaVinci painted the Mona Lisa\nLeonardo Da Vinci was known for his dramatic and expressive artwork\nVincent Van Gogh was a close collaborator of Da Vinci",
+        questionOutput:
+            "Leonardo Da Vinci: painter of the Mona Lisa, Vincent Van Gogh: Worked with Leonardo Da Vinci",
+    },
+    {
+        documentData:
+            "The analytical engine was created by Charles Babbage\nAda Lovelace wrote the first program for the analytical engine",
+        questionOutput:
+            "Charles Babbage: creator of the analytical engine\nAda Lovelace: first programmer of the analytical engine",
+    },
+]
 
-// const exampleFormatterTemplate =
-//     "document data: {documentData}\nFlashcards: {questionOutput}"
-// const examplePrompt = new PromptTemplate({
-//     inputVariables: ["documentData", "questionOutput"],
-//     template: exampleFormatterTemplate,
-// })
+const exampleFormatterTemplate =
+    "document data: {documentData}\nFlashcards: {questionOutput}"
+const examplePrompt = new PromptTemplate({
+    inputVariables: ["documentData", "questionOutput"],
+    template: exampleFormatterTemplate,
+})
 
-// const prompt = new FewShotPromptTemplate({
-//     examples,
-//     examplePrompt,
-//     prefix: "Using the following information, create a list of flashcards that represent key terms in the provided input document. Your output should contain a list of key value pairs separated by commas. Each key value pair includes a term as the key and a description or answer for that term as the value.",
-//     suffix: "Document data: {input}\nFlashcards: ",
-//     inputVariables: ["input"],
-//     exampleSeparator: "\n\n\n",
-//     templateFormat: "f-string",
-// })
+const prompt = new FewShotPromptTemplate({
+    examples,
+    examplePrompt,
+    prefix: "Using the following information, create a list of flashcards that represent key terms in the provided input document. Your output should contain a list of key value pairs separated by newlines. Each key value pair includes a term as the key and a description or answer for that term as the value. The key and value in the flashcard should be separated by a colon. Extract information based on its presence and only extract key terms with clear definitions from the document. Do not make up information on your own.",
+    suffix: "Document data: {input}\nFlashcards: ",
+    inputVariables: ["input"],
+    exampleSeparator: "\n\n\n",
+    templateFormat: "f-string",
+})
 
-// // this is the individual documents prompt
-// const llmChain = new LLMChain({ prompt, llm })
+// this is the individual documents prompt
+const llmChain = new LLMChain({ prompt, llm })
 
-// const chain = new StuffDocumentsChain({
-//     llmChain,
-//     documentVariableName: "input",
-// })
+const chain = new StuffDocumentsChain({
+    llmChain,
+    documentVariableName: "input",
+})
 
-// export default chain
+export default chain
