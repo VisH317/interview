dbf04b72dc992f52596c075533f49478772fcd2f
VisH317 <vt201916384@gmail.com>
2023-07-15 23:08:49 -0400

    setup welcome screen

--- components/Main.vue
+++ components/Main.vue
@@ -7,15 +7,7 @@ const currentNote = useState<string | null>('currentNote')
         <!-- <div class="w-full h-16 bg-slate-50">
 
         </div> -->
-        <div class="flex flex-col w-[1000px] max-w-[80%] gap-10 relative top-80" v-if="currentNote === null">
-            <h1 class="text-slate-800 text-6xl font-semibold relative left-6">Welcome! 🎉</h1>
-            <p class="text-xl text-slate-400 font-normal">Get started by creating a note page or opening one you've already
-                already created. <br /><br />Add information from websites, scan
-                book pages, or open videos to take notes in seconds!
-                <br><br>Prepare in just a couple minutes by creating a quiz or flashcards, or talking to your personal AI
-                tutor. Good luck :)
-            </p>
-        </div>
+        <Welcome v-if="currentNote === null" />
         <div class="flex flex-col w-[1400px] flex flex-col items-center max-w-[80%] min-h-screen gap-10 relative top-10 border-black"
             v-else>
             <NoteEditor />
---
+++ components/Welcome.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+const currentNote = useState<string | null>("currentNote")
+</script>
+
+<template>
+    <div
+        class="w-full h-full flex flex-col items-center relative justify-center"
+    >
+        <div
+            class="flex flex-col w-[700px] max-w-[80%] gap-5 relative"
+            v-if="currentNote === null"
+        >
+            <h1 class="text-slate-800 text-[70px] font-bold relative left-6">
+                Welcome! 🎉
+            </h1>
+            <p class="text-xl text-slate-600 font-light w-[90%]">
+                Get ready to ace your interview in half the time using our
+                tools. Use AI to prepare instantly
+            </p>
+            <div class="h-4" />
+            <h2
+                class="font-['Inter',sans-serif] text-sm text-slate-400 font-bold uppercase"
+            >
+                NOTE NAME
+            </h2>
+            <div
+                class="bg-slate-200 w-[90%] rounded-[10px] border-[1px] border-slate-200 ml-[5%]"
+            >
+                <input
+                    type="text"
+                    class="py-5 px-5 outline-none rounded-[9px] w-full h-full"
+                />
+            </div>
+            <div class="h-4" />
+            <h2
+                class="font-['Inter',sans-serif] text-sm text-slate-400 font-bold uppercase"
+            >
+                Paste in your job description
+            </h2>
+            <div
+                class="bg-slate-200 w-[90%] rounded-[10px] border-[1px] border-slate-200 ml-[5%] h-64"
+            >
+                <textarea
+                    class="py-5 px-5 outline-none rounded-[9px] w-full h-full"
+                    style="resize: none"
+                />
+            </div>
+            <div class="h-2"/>
+            <div class="w-full flex justify-end">
+                <button
+                    class="group relative flex flex-row items-center transition ease-in-out duration-300 hover:-translate-y-1 hover:opacity-[0.85] w-44 h-20 relative -left-5 justify-center"
+                >
+                    <div
+                        class="bg-gradient-to-br from-blue-300 to-pink-300 py-2 px-4 flex justify-center items-center blur-xl w-44 h-full absolute"
+                    ></div>
+                    <div
+                        class="group-hover:shadow-lg duration-300 transition ease-in-out bg-slate-800 text-white text-xl font-light flex justify-center rounded-lg items-center w-36 h-12 cursor-pointer text-center z-20 absolute left-3 top-4"
+                        @click="action"
+                    >
+                        Get Started
+                    </div>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
