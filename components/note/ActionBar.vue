3b0638e7931f61c7075b6ca5bc457bcc17fdb654
VisH317 <vt201916384@gmail.com>
2023-07-16 15:28:05 -0400

    action bar ui complete

--- components/note/ActionBar.vue
+++ components/note/ActionBar.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-
+const hover = ref<boolean>(false)
 </script>
 
 <template>
-    <div class="absolute -bottom-[8%] p-3 w-20 right-[10%] h-[48%] rounded-t-[60px] pt-10 bg-gradient-to-b from-pink-300 to-blue-300">
+    <div class="absolute -bottom-[8%] p-3 right-[5%] h-[48%] rounded-t-[40px] w-20 pt-10 bg-gradient-to-b from-pink-300 to-blue-300 hover:w-60 duration-300" @mouseover="hover=true" @mouseout="hover=false">
         <div class="flex flex-col gap-8">
-            <action-bar-item icon="globe" text="BRUH" :expand="false" />
-            <action-bar-item icon="camera" text="BRUH" :expand="false" />
+            <action-bar-item icon="globe" text="Add Website" :expand="hover" />
+            <action-bar-item icon="camera" text="Add Video" :expand="hover" />
         </div>
-        <hr class="h-[1px] my-8 bg-white border-0 dark:bg-white" />
+        <hr class="h-[1px] my-8 bg-white border-0 dark:bg-white w-[80%] mx-auto" />
         <div class="flex flex-col gap-8">
-            <action-bar-item icon="id-card" text="BRUH" :expand="false" />
-            <action-bar-item icon="check-circle" text="BRUH" :expand="false" />
+            <action-bar-item icon="id-card" text="Flashcards" :expand="hover" />
+            <action-bar-item icon="check-circle" text="Start Quiz" :expand="hover" />
         </div>
     </div>
 </template>
--- components/note/ActionBarItem.vue
+++ components/note/ActionBarItem.vue
@@ -7,10 +7,13 @@ defineProps<{
 </script>
 
 <template>
-    <div class="w-full flex justify-center items-center p-2 text-white">
-        <font-awesome-icon
-            :icon="`fa-solid fa-${icon}`"
-            class="text-3xl text-white"
-        />
+    <div :class="`w-full flex ${expand ? 'justify-start' : 'justify-start'} items-center p-3 py-3 text-white cursor-pointer duration-300 gap-5 hover:bg-[rgba(0,0,0,0.1)] rounded-[15px]`">
+        <div class="w-8">
+            <font-awesome-icon
+                :icon="`fa-solid fa-${icon}`"
+                class="text-3xl text-white mx-auto"
+            />
+        </div>
+        <span :class="`${expand ? 'inline opacity-100' : 'hidden opacity-0'} duration-300 text-xl delay-300 font-medium whitespace-nowrap`">{{ text }}</span>
     </div>
 </template>
