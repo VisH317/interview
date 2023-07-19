83bba4f96b108b7284cc054e92da025f1fd14396
VisH317 <vt201916384@gmail.com>
2023-07-18 20:50:01 -0400

    finish navigator setup

--- components/quiz/QuizNavigator.vue
+++ components/quiz/QuizNavigator.vue
@@ -13,7 +13,9 @@ defineProps<{
     active: number
     formStates: (OEState | MCState)[]
 }>()
-
+defineEmits<{
+    (e: 'activeChange', num: number): void
+}>()
 </script>
 
 <template>
@@ -42,31 +44,32 @@ defineProps<{
                         ? 'bg-transparent border-slate-500 text-slate-300'
                         : 'bg-slate-500 border-slate-500 text-slate-300'
                 } w-10 h-10`"
+                @click="() => $emit('activeChange', ix)"
             >
                 {{ ix+1 }}
             </div>
         </div>
         <div class="flex-none flex w-[80%] flex-col gap-3">
             <div class="flex gap-3 justify-between">
-                <div
+                <button
                     class="group bg-slate-500 w-1/2 justify-center items-center px-8 py-2 h-14 hover:-translate-y-1 duration-300 text-slate-400 font-light text-xl cursor-pointer flex gap-4 rounded-[15px]"
-                    @click="() => { quizModal=false }"
+                    @click="() => $emit('activeChange', active - 1)"
                 >
                     <font-awesome-icon icon="fa-solid fa-caret-left" class="text-white text-4xl"/>
-                </div>
-                <div
+                </button>
+                <button
                     class="group bg-slate-500 w-1/2 justify-center items-center px-8 py-2 h-14 hover:-translate-y-1 duration-300 text-slate-400 font-light text-xl cursor-pointer flex gap-4 rounded-[15px]"
-                    @click="() => { quizModal=false }"
+                    @click="() => $emit('activeChange', active + 1)"
                 >
                     <font-awesome-icon icon="fa-solid fa-caret-right" class="text-white text-4xl"/>
-                </div>
+                </button>
             </div>
-            <div
-                class="group bg-gradient-to-r w-full justify-center from-pink-300 h-14 to-blue-300 items-center px-8 py-3 hover:-translate-y-1 duration-300 text-white font-light text-xl cursor-pointer flex gap-4 rounded-[15px]"
-                @click="() => void createQuiz()"
+            <button
+                class="group bg-gradient-to-r w-full justify-center from-pink-300 h-14 to-blue-300 items-center px-8 py-3 hover:-translate-y-1 duration-300 text-white font-light text-2xl cursor-pointer flex gap-4 rounded-[15px]"
+                @click="() => $emit('activeChange', active - 1)"
             >
                 Submit
-            </div>
+            </button>
         </div>
     </div>
 </template>
--- components/quiz/QuizView.vue
+++ components/quiz/QuizView.vue
@@ -6,11 +6,13 @@ type InProgress = {
 
 type OEState = {
     type: "oe"
+    question: string
     content: string
 }
 
 type MCState = {
     type: "mc"
+    question: string
     content: number
 }
 
@@ -25,15 +27,21 @@ const active = ref<number>(0)
 const formStates = ref<(OEState | MCState)[]>(
     quiz.value?.questions.map((q: string): OEState | MCState => {
         if (q.split(";")[0].split(":")[1].toLowerCase().includes("open"))
-            return { type: "oe", content: "" }
-        return { type: "mc", content: -1 }
+            return { type: "oe", question: q, content: "" }
+        return { type: "mc", question: q, content: -1 }
     })
 )
 </script>
 
 <template>
     <div class="w-full h-full flex p-5">
-        <div class="w-[70%]">{{ quiz.id }}</div>
-        <QuizNavigator :active="active" :formStates="formStates"/>
+        <div class="w-[70%]">
+            <p>Question {{ active+1 }}</p>
+        </div>
+        <QuizNavigator
+            :active="active"
+            :form-states="formStates"
+            @active-change="(ix) => (active = ix<0 ? formStates.length-1 : ix>=formStates.length ? 0 : ix)"
+        />
     </div>
 </template>
