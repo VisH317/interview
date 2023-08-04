aa89ba01c787521b6fae98792034f14cfd35ce97
VisH317 <vt201916384@gmail.com>
2023-08-04 01:25:31 -0400

    fix navbar item positioning

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -13,13 +13,13 @@ const click = () => {
 
 <template>
     <div
-        class="w-full fixed backdrop-blur-lg border-black p-5 text-3xl font-semibold flex flex-row gap-10 justify-center items-center z-50">
-        <div class="flex flex-row gap-10 items-center w-3/12 cursor-pointer">
+        class="w-full fixed backdrop-blur-lg border-black py-5 text-3xl font-semibold flex flex-row justify-center items-center z-50">
+        <div class="flex flex-row gap-10 items-center w-[30%] cursor-pointer">
             <nuxt-img src="/logo.png" height="50px" width="50px" />
             <p class="text-slate-800 font-semibold text-2xl">Interview<span
                     class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300">Sense</span></p>
         </div>
-        <div class="flex flex-row gap-10 items-center w-1/6">
+        <div class="flex flex-row gap-10 items-center w-[30%] justify-end">
             <nuxt-link to="/about"
                 class="text-slate-900 font-light text-xl duration-300 hover:text-slate-400">About</nuxt-link>
             <nuxt-link to="/about"
