6e931a0b2b1a4c51b6f3e8302c12f8dac27a168e
VisH317 <vt201916384@gmail.com>
2023-08-04 07:06:00 -0400

    finally fixed price banner :_)

--- components/PriceBanner.vue
+++ components/PriceBanner.vue
@@ -14,22 +14,8 @@ defineProps<{
             <h1 :class="`text-${main ? 'white' : 'slate-800'} text-5xl font-semibold text-center`">${{ price }} /
                 month</h1>
         </div>
-        <div v-for="(point) in points" class="flex-none">
-            <div class="flex justify-start gap-5 items-center">
-                <font-awesome-icon icon="fa-solid fa-check" :class="`${main ? 'text-white' : 'text-slate-500'} text-xl`"/>
-                <div>
-                    <p :class="`font-light text-2xl ${main ? 'text-white' : 'text-slate-600'}`">{{ point }}</p>
-                </div>
-            </div>
-        </div>
-        <div v-for="(ex) in exes" class="flex-none flex justify-start gap-5 items-center">
-            <div class="flex justify-start gap-5 items-center">
-                <font-awesome-icon icon="fa-solid fa-x" :class="`${main ? 'text-white' : 'text-slate-400'} text-xl`"/>
-                <div>
-                    <p :class="`font-light text-2xl ${main ? 'text-white' : 'text-slate-400'}`">{{ ex }}</p>
-                </div>
-            </div>
-        </div>
+        <PriceListItem v-for="point in points" ischeck :main="main" :text="point" />
+        <PriceListItem v-for="ex in exes" :ischeck="false" :main="main" :text="ex" />
         <div class="grow" />
         <div class="w-full flex justify-center">
             <Button text="Join" :action="() => console.log('action')" />
---
+++ components/PriceListItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+    main: boolean
+    text: string
+    ischeck: boolean
+}>()
+</script>
+
+<template>
+    <div class="flex-none flex justify-start gap-6 items-center">
+        <div>
+            <font-awesome-icon :icon="`fa-solid ${ischeck ? 'fa-check' : 'fa-x'}`" :class="`${main ? 'text-white' : 'text-slate-500'} text-xl`"/>
+        </div>
+        <p :class="`font-light text-2xl ${main ? 'text-white' : 'text-slate-600'}`">{{ text }}</p>
+    </div>
+</template>
