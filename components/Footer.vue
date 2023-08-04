77dcb714c52dba9ebe3bf4be068b168189dfbf77
VisH317 <vt201916384@gmail.com>
2023-08-04 05:19:38 -0400

    fix footer formatting

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <div class="w-full bg-slate-300 p-10 py-20 flex justify-center items-center h-[30vh]">
+    <div class="w-full bg-slate-300 p-10 py-20 flex justify-center items-center h-[280px]">
         <div class="w-[30%] border-r-2 h-[100%] flex items-center justify-center border-slate-400">
-            <p class="text-slate-800 font-bold text-6xl text-center">Interview<span
+            <p class="text-slate-800 font-bold text-4xl text-center">Interview<span
                     class="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300">Sense</span></p>
         </div>
         <div class="w-[30%] flex flex-col items-center justify-center gap-10">
