64e35009bd46f28944cc63e89a9aee07e2f66f65
VisH317 <vt201916384@gmail.com>
2023-08-04 06:05:02 -0400

    setup infinite testimonial scroll

--- components/testimonial/Testimonial.vue
+++ components/testimonial/Testimonial.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="w-1/3 h-full p-5 flex flex-col gap-4 shrink-0">
+    <div class="w-[12.5%] h-full p-10 flex flex-col gap-4 shrink-0">
         <nuxt-img :src="imgPath" height="50px" width="50px" class="rounded-[50%]"/>
         <p class="font-semibold text-xl text-slate-800">"{{ quote }}"</p>
         <p class="text-lg font-light text-slate-500">— {{ desc }}</p>
--- components/testimonial/TestimonialList.vue
+++ components/testimonial/TestimonialList.vue
@@ -3,12 +3,37 @@
 </script>
 
 <template>
-    <div class="w-full bg-slate-300 h-80 p-5 flex items-center">
-        <div class="w-[80%] ml-[10%] overflow-hidden flex gap-4 ">
-        <Testimonial imgPath="/aized.jpg" quote="Very good product with amazing experience! Can't wait to use!" desc="Aized Sethna, Cofounder & CEO @ Sticky Cards" />
-        <Testimonial imgPath="/dhruv.jpg" quote="Amazing experience using and would definitely recommend!" desc="Dhruv Kulkarni, Cofounder @ AI Entrepreneurs at Berkeley" />
-        <Testimonial imgPath="/aized.jpg" quote="Very good product with amazing experience! Can't wait to use!" desc="Aized Sethna, Cofounder & CEO @ Sticky Cards" />
-        <Testimonial imgPath="/dhruv.jpg" quote="Amazing experience using and would definitely recommend!" desc="Dhruv Kulkarni, Cofounder @ AI Entrepreneurs at Berkeley" />
+    <div class="w-full bg-slate-300 h-80 p-5 overflow-hidden">
+        <div class="flex inner h-full" :style="`width: ${33.333333333*8}%`">
+            <Testimonial imgPath="/aized.jpg" quote="Very good product with amazing experience! Can't wait to use!" desc="Aized Sethna, Cofounder & CEO @ Sticky Cards" />
+            <Testimonial imgPath="/dhruv.jpg" quote="Amazing experience using and would definitely recommend!" desc="Dhruv Kulkarni, Cofounder @ AI Entrepreneurs at Berkeley" />
+            <Testimonial imgPath="/aized.jpg" quote="Very good product with amazing experience! Can't wait to use!" desc="Aized Sethna, Cofounder & CEO @ Sticky Cards" />
+            <Testimonial imgPath="/dhruv.jpg" quote="Amazing experience using and would definitely recommend!" desc="Dhruv Kulkarni, Cofounder @ AI Entrepreneurs at Berkeley" />
+            <Testimonial imgPath="/aized.jpg" quote="Very good product with amazing experience! Can't wait to use!" desc="Aized Sethna, Cofounder & CEO @ Sticky Cards" />
+            <Testimonial imgPath="/dhruv.jpg" quote="Amazing experience using and would definitely recommend!" desc="Dhruv Kulkarni, Cofounder @ AI Entrepreneurs at Berkeley" />
+            <Testimonial imgPath="/aized.jpg" quote="Very good product with amazing experience! Can't wait to use!" desc="Aized Sethna, Cofounder & CEO @ Sticky Cards" />
+            <Testimonial imgPath="/dhruv.jpg" quote="Amazing experience using and would definitely recommend!" desc="Dhruv Kulkarni, Cofounder @ AI Entrepreneurs at Berkeley" />
         </div>
     </div>
 </template>
+
+<style scoped>
+.inner {
+    display: flex;
+    width: fit-content;
+    animation-name: loop;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: normal;
+    animation-duration: 14000ms;
+}
+
+@keyframes loop {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
+}
+</style>
